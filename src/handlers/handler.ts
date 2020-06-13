412e52f04e9363bf6d1cbdb3d2ed537dc39d5b0b
William Chen <kis87988@gmail.com>
2020-06-13 16:53:48 -0700

    feat: bot handler basis

---
+++ src/handlers/handler.ts
@@ -0,0 +1,144 @@
+import { logger } from "../logger";
+import { Wechaty } from "wechaty";
+import { cn2tw, tw2cn } from "cjk-conv";
+import * as util from "../util";
+import {
+  Contact as WechatyContact,
+  ContactSelf as WechatyContactSelf,
+  Message as WechatyMessage,
+  Room as WechatyRoom,
+  Friendship as WechatyFriendship,
+  RoomInvitation as WechatyRoomInvitation,
+} from "wechaty/dist/src/user/";
+
+export {
+  WechatyContact,
+  WechatyContactSelf,
+  WechatyMessage,
+  WechatyRoom,
+  WechatyFriendship,
+  WechatyRoomInvitation,
+};
+
+export abstract class Handler {
+  // TODO(WilliamC, kis87988): add Wechaty cache for findAll() room
+  protected bot: Wechaty;
+  constructor(bot: Wechaty, ...args: any[]) {
+    this.bot = bot;
+  }
+
+  protected async sleep(ms: number = util.getRandomIntInclusive(3000, 5000)) {
+    await util.sleep(ms);
+  }
+
+  protected getRandomIntInclusive(min: number, max: number) {
+    return util.getRandomIntInclusive(min, max);
+  }
+
+  protected isTcText(text: string) {
+    const textTw = cn2tw(text);
+    return textTw == text;
+  }
+
+  protected async getRoomListToString(
+    roomList: WechatyRoom[],
+    displayId: boolean = false
+  ) {
+    if (roomList.length == 0) return "";
+    let roomItemList: string = "rooms list:\n";
+
+    try {
+      for (let i = 0; i < roomList.length; i++) {
+        const roomTopic = await roomList[i].topic();
+        if (displayId)
+          roomItemList += i + 1 + `. ${roomTopic}  id: ${roomList[i].id}\n`;
+        else roomItemList += i + 1 + `. ${roomTopic}\n`;
+      }
+
+      return roomItemList;
+    } catch (err) {
+      logger.error("Bot", "getRoomList() exception: " + err.stack);
+      return "Can not get rooms list";
+    }
+  }
+
+  protected async getRoomList(regexpGroup: RegExp[] = []) {
+    try {
+      const roomList: WechatyRoom[] = await this.bot.Room.findAll();
+
+      // means get all rooms
+      if (regexpGroup.length == 0) return roomList;
+
+      const filterRoomList: WechatyRoom[] = [];
+      for (const room of roomList) {
+        const roomTopic: string = await room.topic();
+        const roomTopicCn: string = tw2cn(roomTopic);
+        for (const regexp of regexpGroup)
+          if (regexp.test(roomTopicCn)) filterRoomList.push(room);
+      }
+      return filterRoomList;
+    } catch (error) {
+      logger.error("Bot", "getRoomList() exception: " + error.stack);
+      return [];
+    }
+  }
+
+  protected async addUserToRoom(contact: WechatyContact, room: WechatyRoom) {
+    const roomMaxSize = 500;
+    let roomMemberCount: number = 0;
+    let hasMember: boolean = false;
+    try {
+      roomMemberCount = await room
+        .memberAll()
+        .then((memberList: any[]) => memberList.length)
+        .catch(() => 0);
+      hasMember = await room.has(contact);
+    } catch (error) {
+      logger.error(
+        "Bot",
+        "addUserToRoom() call room.memberAll(),room.has() exception: " +
+          error.stack
+      );
+      throw new Error("不好意思，邀請過程中發生點問題");
+    }
+
+    if (hasMember) throw new Error("不好意思，你已經在想加的群中了");
+    if (roomMemberCount >= roomMaxSize)
+      throw new Error("不好意思，想加的群超過500人了");
+    await this.sleep();
+    try {
+      room.add(contact);
+    } catch (error) {
+      logger.error(
+        "Bot",
+        "addUserToRoom() call room.add() exception: " + error.stack
+      );
+      throw new Error("不好意思，邀請過程中發生點問題，請聯絡管理員");
+    }
+  }
+
+  protected async roomMatch(text: string, roomList: WechatyRoom[]) {
+    const trimText: string = text.trim();
+    const trimTextCn: string = tw2cn(trimText);
+
+    // number only
+    if (/\d+/gim.test(trimText)) {
+      const roomIndex = +trimText - 1;
+      if (roomIndex < roomList.length) return roomList[roomIndex];
+    }
+
+    // text match room.topic()
+
+    const textRegexp: RegExp = RegExp(trimTextCn, "gim");
+    for (const room of roomList) {
+      const roomTopic: string = await room.topic();
+      const roomTopicCn = tw2cn(roomTopic);
+      if (textRegexp.test(roomTopicCn)) return room;
+    }
+
+    // nothing match
+    return null;
+  }
+
+  public abstract listener(...args: any[]): void;
+}
---
+++ src/main.ts
@@ -0,0 +1,73 @@
+require("dotenv").config();
+
+import { logger } from "./logger";
+import * as handlers from "./handlers";
+
+import { Wechaty } from "wechaty";
+
+function botInstantiate(): Wechaty {
+  if (!process.env.WECHATY_PUPPET_PADPLUS_TOKEN) {
+    logger.error(
+      `env: "WECHATY_PUPPET_PADPLUS_TOKEN" not define, please define token in .env file`
+    );
+    throw Error(`token not define`);
+  }
+
+  const bot = new Wechaty({
+    name: "FHDA-wechat-Bot",
+    puppet: "wechaty-puppet-padplus",
+    puppetOptions: {
+      token: process.env.WECHATY_PUPPET_PADPLUS_TOKEN,
+    },
+  });
+
+  logger.info(`wechaty version:${bot.version()},`);
+  return bot;
+}
+
+async function main() {
+  logger.info(`Start bot`);
+
+  const bot: Wechaty = botInstantiate();
+
+  const botHandler = {
+    error: new handlers.Error_(bot),
+    ready: new handlers.Ready(bot),
+    start: new handlers.Start(bot),
+    stop: new handlers.Stop(bot),
+    scan: new handlers.Scan(bot),
+    login: new handlers.Login(bot),
+    logout: new handlers.Logout(bot),
+    message: new handlers.Message(bot),
+    friendship: new handlers.Friendship(bot),
+    roomJoin: new handlers.RoomJoin(bot),
+    roomLeave: new handlers.RoomLeave(bot),
+    roomTopic: new handlers.RoomTopic(bot),
+    roomInvite: new handlers.RoomInvite(bot),
+    heartbeat: new handlers.Heartbeat(bot),
+  };
+
+  // bind all listeners
+  bot
+    .on("error", (...args) => botHandler.error.listener(...args))
+    .on("ready", (...args) => botHandler.ready.listener(...args))
+    .on("start", (...args) => botHandler.start.listener(...args))
+    .on("stop", (...args) => botHandler.stop.listener(...args))
+    .on("scan", (...args) => botHandler.scan.listener(...args))
+    .on("login", (...args) => botHandler.login.listener(...args))
+    .on("logout", (...args) => botHandler.logout.listener(...args))
+    .on("message", (...args) => botHandler.message.listener(...args))
+    .on("friendship", (...args) => botHandler.friendship.listener(...args))
+    .on("room-join", (...args) => botHandler.roomJoin.listener(...args))
+    .on("room-leave", (...args) => botHandler.roomLeave.listener(...args))
+    .on("room-topic", (...args) => botHandler.roomTopic.listener(...args))
+    .on("room-invite", (...args) => botHandler.roomInvite.listener(...args))
+    .on("heartbeat", (...args) => botHandler.heartbeat.listener(...args));
+
+  // bot start
+  bot.start();
+}
+
+main()
+  .then()
+  .catch((e) => console.error(e));
