6bb29d0d57e88af84630b9e83da2f702dccff3c9
William Chen <kis87988@gmail.com>
2020-06-14 23:51:11 -0700

    feat: add MongoConnect basis class

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -32,3 +32,65 @@ abstract class Connection {
     await this.open();
   }
 }
+
+export class MongoConnection extends Connection {
+  protected dbClient: mongodb.MongoClient;
+  private options?: mongodb.MongoClientOptions;
+  private db?: mongodb.Db;
+  private getMongoDbUrl(): string {
+    let mongoDbUrl: string = `mongodb://`;
+    if (this.dbAuth.user) {
+      mongoDbUrl += this.dbAuth.user;
+      if (this.dbAuth.password) mongoDbUrl += `:${this.dbAuth.password}`;
+      mongoDbUrl += `@${this.dbAuth.url}`;
+    } else {
+      mongoDbUrl += this.dbAuth.url;
+    }
+    return mongoDbUrl;
+  }
+
+  constructor(dbAuth: DbAuth, options?: mongodb.MongoClientOptions) {
+    super(dbAuth);
+    this.dbAuth = dbAuth;
+    this.options = options;
+    const mongoDbUrl: string = this.getMongoDbUrl();
+    this.dbClient = new mongodb.MongoClient(mongoDbUrl, this.options);
+  }
+
+  protected async open(): Promise<boolean> {
+    if (this.dbClient.isConnected()) {
+      return true;
+    }
+    logger.info("start opening mongodb connection");
+    try {
+      await this.dbClient.connect();
+    } catch (error) {
+      logger.error(`Connect to mongoDB error: ${error}`);
+      return false;
+    }
+
+    this.db = this.dbClient.db(this.dbAuth.dbName);
+
+    logger.info("mongodb connection opened");
+    return true;
+  }
+
+  protected async close(force?: boolean): Promise<void> {
+    logger.info("start closing mongodb connection");
+    await this.dbClient.close(force);
+    this.db = undefined;
+    logger.info("closed mongodb connection");
+  }
+
+  public async isConnect(): Promise<boolean> {
+    return this.dbClient.isConnected();
+  }
+
+  public getDb(): mongodb.Db | undefined {
+    return this.db;
+  }
+
+  public getClient(): mongodb.MongoClient {
+    return this.dbClient;
+  }
+}
